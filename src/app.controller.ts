4edec76e62f01c1892701d92263b2b8085babc91
Ulan <ulikebot@gmail.com>
2024-03-27 04:50:48 +0600

    Get decorator string return

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,19 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Param, ParseArrayPipe, ParseIntPipe, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { stringify } from 'querystring';
+import { StringDecoder } from 'string_decoder';
+import { pipeline } from 'stream';
 
-@Controller()
+@Controller('app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
  @Get()
   getHello(): string {
     return this.appService.getHello();
   }
-  @Get("Users")
-  getTest(){
-    return "User requests!"
+  @Get("get/:user")
+  getTest(@Param('user')user:string){
+    return user ;
   }
   @Get("Guest")
   notest(){
