ec08c7379c65de043e84ef2fba6c3e6c9b61cfa6
Ulan <ulikebot@gmail.com>
2024-04-03 03:40:29 +0600

    module generate test

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { RegistrationModule } from './registration/registration.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, RegistrationModule],
   controllers: [AppController],
   providers: [AppService],
 })
